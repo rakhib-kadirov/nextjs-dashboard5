d4b9ba007e250dd6de96dd7c4dee7f8b0c3a810b
Rakhib Kadirov <RahiBihar@yandex.ru>
2025-02-26 19:34:47 +0300

    rahzo_26022025_1934

--- app/api/debug/session/route.ts
+++ app/api/debug/session/route.ts
@@ -2,9 +2,17 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const session = await auth();  // ⬅ Принудительно вызываем session()
+    try {
+        const session = await auth();  // ⬅ Принудительно вызываем session()
+        console.log("DEBUG API - Session:", session); // ✅ Логируем сессию
 
-    console.log("DEBUG API - Session:", session); // ✅ Логируем сессию
+        return NextResponse.json(session);
+    } catch (error: any) {
+        console.error("Ошибка: Ошибка session!", error.message)
 
-    return NextResponse.json(session);
+        return NextResponse.json(
+            { success: true, error: 'Ошибка сервера. Session' },
+            { status: 500 }
+        )
+    }
 }
