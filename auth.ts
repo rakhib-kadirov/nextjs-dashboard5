cd09df6a8bcd976a60d4f864daee14bf56e842d6
Rakhib Kadirov <RahiBihar@yandex.ru>
2025-02-27 23:49:06 +0300

    rahzo_27022025_2349

--- auth.ts
+++ auth.ts
@@ -22,7 +22,7 @@ async function getUser(login: string): Promise<User | undefined> {
     // const session = await auth()
     try {
         console.log('Login: ', login)
-        const user = prisma.users.findUnique({
+        const user = await prisma.users.findUnique({
             where: {
                 // id: parseInt(session?.user.id as string),
                 login: login
