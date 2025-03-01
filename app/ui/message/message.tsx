82db33477a24a78533f2b5849082692c382a665d
Rakhib Kadirov <RahiBihar@yandex.ru>
2025-03-01 12:17:47 +0300

    rahzo_01032025_1217

--- app/ui/message/message.tsx
+++ app/ui/message/message.tsx
@@ -7,6 +7,7 @@ import { useSession } from "next-auth/react";
 import clsx from "clsx";
 import Image from "next/image";
 import { format } from "date-fns"
+import Pusher from 'pusher-js'
 
 // const socket = io('http://localhost:3001', {
 //     path: '/api/socket.io',
@@ -23,13 +24,17 @@ interface Message {
     profile_photo: string;
 }
 
-const socket = io("wss://rahzo.vercel.app:3001")
+// const socket = io("wss://rahzo.vercel.app:3001")
 // socket = io("http://26.137.137.103:3001")
+const pusher = new Pusher("PUSHER_APP_KEY", {
+    cluster: "eu"
+})
+const channel = pusher.subscribe('chat')
 
 interface User {
     id: string;
-    first_name?: string | null;
-    last_name?: string | null;
+    first_name?: string;
+    last_name?: string;
 }
 
 interface Message {
@@ -50,7 +55,6 @@ export default function Message() {
     const first_name = user?.first_name
     const last_name = user?.last_name
 
-
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -69,15 +73,19 @@ export default function Message() {
         }
         fetchData()
 
-        socket.on('newMessage', (message) => {
+        channel.bind('message', (message: Message) => {
             setMessages((prev) => [...prev, message])
         })
+        // socket.on('newMessage', (message) => {
+        //     setMessages((prev) => [...prev, message])
+        // })
 
-        return () => { socket.off('newMessage') }
+        return () => { channel.cancelSubscription() }
     }, [])
 
     const sendMessage = () => {
-        socket.emit('sendMessage', { text, userId, first_name, last_name })
+        // socket.emit('sendMessage', { text, userId, first_name, last_name })
+        channel.emit('message', { text, userId, first_name, last_name })
         setText('')
     };
 
@@ -90,69 +98,69 @@ export default function Message() {
         <>
             <div className="w-3/4">
                 {/* <section className="flex flex-col"> */}
-                    <div className="flex flex-row box-content h-[calc(100vh-50px)] w-full">
-                        <div className="w-2/5">
-                            <div className="bg-white shadow-md">
-                                <h3 className="text-lg font-semibold">Чаты</h3>
-                                <Search />
-                            </div>
-                            Другие
+                <div className="flex flex-row box-content h-[calc(100vh-50px)] w-full">
+                    <div className="w-2/5">
+                        <div className="bg-white shadow-md">
+                            <h3 className="text-lg font-semibold">Чаты</h3>
+                            <Search />
                         </div>
-                        <div className="grid w-full">
-                            <div className="grid gap-3 p-[10px] scroll-smooth overflow-y-auto bg-red-200">
-                                {messages.map((msg) => {
-                                    const currentDate = format(msg.createdAt, 'H:mm')
-                                    return (
-                                        <div className="inline-block w-full" key={msg.id}>
-                                            <div id="blockRight" className={msg.userId.toString() === session?.user?.id ? "text-right" : "text-left"}>
+                        Другие
+                    </div>
+                    <div className="grid w-full">
+                        <div className="grid gap-3 p-[10px] scroll-smooth overflow-y-auto bg-red-200">
+                            {messages.map((msg) => {
+                                const currentDate = format(msg.createdAt, 'H:mm')
+                                return (
+                                    <div className="inline-block w-full" key={msg.id}>
+                                        <div id="blockRight" className={msg.userId.toString() === session?.user?.id ? "text-right" : "text-left"}>
+                                            <div className={clsx(
+                                                "inline-flex items-end gap-3",
+                                                {
+                                                    "flex-row-reverse": msg.userId.toString() === session?.user?.id
+                                                }
+                                            )}>
+                                                <div className="h-full items-end">
+                                                    <Image
+                                                        className="rounded-full w-[40px] h-[40px]"
+                                                        src={msg.profile_photo ? msg.profile_photo : "/stock.png"}
+                                                        alt=""
+                                                        width={40}
+                                                        height={40}
+                                                    />
+                                                </div>
                                                 <div className={clsx(
-                                                    "inline-flex items-end gap-3",
-                                                    {
-                                                        "flex-row-reverse": msg.userId.toString() === session?.user?.id
-                                                    }
+                                                    "relative bg-red-300 rounded-[8px] min-w-[60px] min-h-[60px] gap-2 pl-[8px] pr-[8px] pt-[4px] pb-[4px] text-left",
                                                 )}>
-                                                    <div className="h-full items-end">
-                                                        <Image
-                                                            className="rounded-full w-[40px] h-[40px]"
-                                                            src={msg.profile_photo ? msg.profile_photo : "/stock.png"}
-                                                            alt=""
-                                                            width={40}
-                                                            height={40}
-                                                        />
-                                                    </div>
-                                                    <div className={clsx(
-                                                        "relative bg-red-300 rounded-[8px] min-w-[60px] min-h-[60px] gap-2 pl-[8px] pr-[8px] pt-[4px] pb-[4px] text-left",
-                                                    )}>
-                                                        <div className="grid gap-2">
-                                                            <strong className="text-[14px]">{msg.first_name}</strong>
-                                                            <div className="flex">
-                                                                <div className="pr-[40px] max-w-[300px]">
-                                                                    <p>{msg.text}</p>
-                                                                </div>
+                                                    <div className="grid gap-2">
+                                                        <strong className="text-[14px]">{msg.first_name}</strong>
+                                                        <div className="flex">
+                                                            <div className="pr-[40px] max-w-[300px]">
+                                                                <p>{msg.text}</p>
                                                             </div>
                                                         </div>
-                                                        <span className="absolute bottom-[2px] right-[8px] text-[12px]">{currentDate}</span>
                                                     </div>
+                                                    <span className="absolute bottom-[2px] right-[8px] text-[12px]">{currentDate}</span>
                                                 </div>
                                             </div>
                                         </div>
-                                    )
-                                })}
-                            </div>
-                            <div className="flex items-center gap-2 p-2 bg-white border-t shadow-md">
-                                <input
-                                    className="flex-grow p-2 border rounded-lg"
-                                    type="text"
-                                    value={text}
-                                    onChange={(e) => setText(e.target.value)}
-                                    placeholder="Напишите сообщение..."
-                                />
-                                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={sendMessage}>
-                                    Отправить
-                                </button>
-                            </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="flex items-center gap-2 p-2 bg-white border-t shadow-md">
+                            <input
+                                className="flex-grow p-2 border rounded-lg"
+                                type="text"
+                                value={text}
+                                onChange={(e) => setText(e.target.value)}
+                                placeholder="Напишите сообщение..."
+                            />
+                            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={sendMessage}>
+                                Отправить
+                            </button>
                         </div>
                     </div>
+                </div>
                 {/* </section> */}
             </div>
         </>
